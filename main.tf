9cfcc3d88ead2e39b2bffbf9502e466415dc0ccc
pedroadias <pedroadias@external.sprintersports.com>
2024-11-09 19:36:47 +0000

    add bucket

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "my-bucket" {
-  name                     = "bucket-1-PedroDias-Demo"
+  name                     = "bucket-1-PedroDias-Dem2o"
   project                  = "tt-dev-001"
   location                 = "US"
   force_destroy            = true
