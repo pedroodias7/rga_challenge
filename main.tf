775bcab891adf9dbe1f385ccc93a74c7fcac7457
pedroadias <pedroadias@external.sprintersports.com>
2024-11-10 15:23:39 +0000

    teste

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "my-bucket" {
-  name                     = "bucket-1-PedroDias-Dems2o"
+  name                     = "bucket-1-pedroias-ems2o"
   project                  = "tt-dev-001"
   location                 = "US"
   force_destroy            = true
